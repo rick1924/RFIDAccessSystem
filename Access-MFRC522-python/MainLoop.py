74084ed4cdf6bb657422b2ecf815caa7d168bc3e
Ricardo Rivera <rrivera2497@gmail.com>
2020-05-12 19:08:55 +0200

    Now use Python 3. Use LCD screen

--- Access-MFRC522-python/MainLoop.py
+++ Access-MFRC522-python/MainLoop.py
@@ -1,20 +1,19 @@
-# Modified by Ricardo Rivera on May 1st, 2019
+# Created by Ricardo Rivera
+# Last Edit: Ricardo Rivera, August 4th 2019
 #   This file uses the MFRC522-Python library created by Mario Gomez that is
 #   available for download at https://github.com/mxgxw/MFRC522-python
 
 # This program uses a RFID reader and RFID tags to authenticate a student and enable the operation of a
-# machine. Each tag has written in it a unique identifier ("UID") and the user's Booked ID, all of
+# machine. Each tag has written in it a unique identifier "UID" and the student's Booked ID, all of
 # which is retrieved by the RFID reader and temporarily stored in memory. The program then evaluates whether
 # the tag belongs to an administrator, or the tag belongs to a user.
 # First, the script checks the local administrator table, and checks whether the UID of the tag, matches any
 # of the records inside the table. If the UID of the tag does not exist in the table, then it will make an API
-# call to Booked, and use the Booked ID to find the information about the user. If the user has an active
+# call to Booked, and use the Booked ID to find the information about the student. If the user has an active
 # reservation at the time the tag is read, a relay then enables the machine. If the user does not have a reservation
 # at that time, access will not be granted. If the tag that was read belongs to an admin, it will automatically grant
 # access, no matter whether a reservation was made or not.
-# Every time a user is allowed access to the machine, the script will log the UID of the user in a separate file.
-
-# Modify all code you see written in CAPS with an underscore with your information. Be careful not to modify the SQL code that is also in caps.
+# Every time a user is allowed access to the machine, the script with log the UID of the user in a separate file.
 
 import RPi.GPIO as GPIO
 import MFRC522
@@ -29,6 +28,25 @@ import logging
 import requests
 import json
 import os
+import sys
+
+import board
+import busio
+import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
+
+# Function that makes an API call to our server's database, extracts the admin table, and stores it locally.
+def download_admin():
+    try:
+        engine = create_engine("mysql+pymysql://SQL_USERNAME:SQL_PASSWORD@MYSQL_SERVER_IP_ADDRESS/YOUR_DATABASE_NAME")
+        admin_table = engine.execute("SELECT * FROM YOUR_ADMIN_TABLE_NAME").fetchall()
+        admin_frame = pd.DataFrame(admin_table, columns=["Name","UID","id"])
+        admin_frame.to_csv("/home/pi/YOUR_PROJECT_FOLDER/AdminTags.csv", index = False)
+        logging.info("The admin tag table was successfully updated")    # Creates a log entry
+        print ("The admin tag table was successfully updated.")
+    except exc.OperationalError:
+        logging.info("The admin tag table could not be updated.")   # Creates a log entry
+        print ("The admin tag table could not be updated.")
+        return False
 
 # The following function gets the authentication headers by making a "POST" call to Booked. We will use these headers to make all successive calls.
 def get_headers():
@@ -43,42 +61,24 @@ def get_headers():
     admin_user_id = authenticate_response_json["userId"]
     return {"X-Booked-SessionToken": session_token, "X-Booked-UserId": admin_user_id}
 
-# Function that makes an API call to our server's database, extracts the admin table, and stores it locally.
-def download_admin():
-    try:
-        engine = create_engine("mysql+pymysql://SQL_USERNAME:SQL_PASSWORD@MYSQL_SERVER_IP_ADDRESS/YOUR_DATABASE_NAME")
-        admin_table = engine.execute("SELECT * FROM YOUR_ADMIN_TABLE_NAME").fetchall()
-        admin_frame = pd.DataFrame(admin_table, columns=["Name","UID","id"])
-        admin_frame.to_csv("/home/pi/YOUR_PROJECT_FOLDER/AdminTags.csv", index = False)
-        logging.info("The admin tag table was successfully updated")    # Creates a log entry
-        print "The admin tag table was successfully updated."
-    except exc.OperationalError:
-        logging.info("The admin tag table could not be updated.")   # Creates a log entry
-        print "The admin tag table could not be updated."
-
-# Function that closes all communication between the Pi and the modules.
-def end_read(signal, frame):
-    global continue_reading
-    print "Ctrl+C captured, ending read"
-    continue_reading = False
-    GPIO.cleanup()
-    os._exit(0)
-
-# Function that compares the time a certain user made a reservation in Booked, with the time that it is now.
-def compare_times_resource_id(user_id_int, auth_headers, resource_id):
+# Function that gets the next active reservation by the given user.
+def get_user_reservation(user_id_int, auth_headers):
     reservations_url = "http://YOUR_BOOKED_DOMAIN/Web/Services/index.php/Reservations/"
     params = {"userId": user_id_int}
-    get_reservations = requests.get(reservations_url, headers=auth_headers, params=params)  # Gets all the reservations made for a specific user.
-    get_reservations_json = get_reservations.json()  # Converts into a JSON object.
+    user_reservations = requests.get(reservations_url, headers=auth_headers, params=params)  # Gets all the reservations made for a specific user.
+    user_reservations_json = user_reservations.json()  # Converts into a JSON object.
+    first_reservation = user_reservations_json["reservations"][0] # We only have to check the first entry "0" since booked accommodates the reservations chronologically.
+    return first_reservation
 
-    machine_id = get_reservations_json["reservations"][0]["resourceId"]
-
-    time_a = get_reservations_json["reservations"][0]["startDate"]  # We only have to check the first entry ([0]) since Booked returns the reservations in chronological order.
-    time_b = get_reservations_json["reservations"][0]["endDate"]
+# Function that compares the time a certain user made a reservation in Booked, with the time that it is now.
+def compare_times_resource_id(user_id_int, resource_id, reservation, auth_headers):
+    machine_id = reservation["resourceId"]
+    time_a = reservation["startDate"]
+    time_b = reservation["endDate"]
     year_a, month_a, day_a, hour_a, minute_a = int(time_a[0:4]), int(time_a[5:7]), int(time_a[8:10]), int(time_a[11:13]), int(time_a[14:16])
     year_b, month_b, day_b, hour_b, minute_b = int(time_b[0:4]), int(time_b[5:7]), int(time_b[8:10]), int(time_b[11:13]), int(time_b[14:16])
 
-    allowed_minutes = 15    # We allow users to access the machine 15 minutes before their reservation time.
+    allowed_minutes = 10
     allowed_time = datetime.timedelta(minutes = allowed_minutes)
     start_datetime = datetime.datetime(year_a, month_a, day_a, hour_a, minute_a) - allowed_time
     end_datetime = datetime.datetime(year_b, month_b, day_b, hour_b, minute_b)
@@ -103,19 +103,58 @@ def find_booked_uid(user_id_int, auth_headers):
     user_url = "http://YOUR_BOOKED_DOMAIN/Web/Services/index.php/Users/%s" % user_id_int
     get_user_info = requests.get(user_url, headers=auth_headers)
     get_user_info_json = get_user_info.json()
-    return int(get_user_info_json["customAttributes"][0]["value"])
+    try:
+        return int(get_user_info_json["customAttributes"][0]["value"])
+    except KeyError:
+        return False
 
-# The function determines whether the tag owner is a regular user or an admin.
-def admin_or_user(uid_int, user_id_int):
-    if (find_if_admin(uid_int) == True):
-        return True
+# The function determines the identification of the user. Whether the user is an admin, has a reservation on this machine at this particular instant, is
+# registered in the systerm but does not have an active reservation, or simply the tag is not registered in the system.
+def find_identification(uid_int, user_id_int, auth_headers):
+    if find_booked_uid(user_id_int, auth_headers) == uid_int:
+        try:
+            reservation = get_user_reservation(user_id_int, auth_headers)
+            if compare_times_resource_id(user_id_int, resource_id, reservation, auth_headers) == True:
+                identification = confirmed_student
+            else:
+                identification = rejected_student
+        except IndexError:
+            identification = rejected_student
+            reservation = {}
     else:
-        headers = get_headers()
-        if (find_booked_uid(user_id_int, headers) == uid_int and compare_times_resource_id(user_id_int, headers, resource_id) == True):
-            return True
+        reservation = {}
+        if find_if_admin(uid_int) == True:
+            identification = admin
+        else:
+            identification = unknown
+    return reservation, identification
 
-# This line says the Pi belongs to the resource with id __. Only reservations made for this machine will work with this Pi.
-resource_id = "RESOURCE_ID"
+# Function that writes in Booked the time the student with a reservation first begins using the machine.
+def check_in(identification, user_id_int, reservation, auth_headers):
+    if identification == confirmed_student:
+        ref_num =  reservation["referenceNumber"]
+        checkin_url = "http://YOUR_BOOKED_DOMAIN/Web/Services/index.php/Reservations/{}/CheckIn".format(ref_num)
+        requests.post(checkin_url, headers=auth_headers)
+
+# Function that writes in Booked the time he student with a reservation leaves the machine.
+def check_out(identification, user_id_int, reservation, auth_headers):
+    if identification == confirmed_student:
+        ref_num = reservation["referenceNumber"]
+        checkout_url = "http://YOUR_BOOKED_DOMAIN/Web/Services/index.php/Reservations/{}/CheckOut".format(ref_num)
+        requests.post(checkout_url, headers=auth_headers)
+
+# Function that closes all communication between the Pi and the modules.
+def end_read(signal, frame):
+    global continue_reading
+    print "Ctrl+C captured, ending read"
+    continue_reading = False
+    GPIO.cleanup()
+    sys.exit(1)
+
+# This line retrieves the resource id that the Pi belongs. It does it by finding the host name, and then removing the first two letters from this string, therefore giving
+# the resource id.  Only reservations made for this machine will work with this Pi.
+pi_hostname = os.uname()[1] # This command gets the hostname of the Pi. Recall that the hostname is different from the username.
+resource_id = pi_hostname[2:]
 
 # The following two lines configure the log file were all relevant information will be stored. "INFO" is used to provide good but not too detailed information.
 log_filename = "/home/pi/YOUR_PROJECT_FOLDER/LogMainLoop.log"
@@ -124,10 +163,16 @@ logging.basicConfig(filename=log_filename, level=logging.INFO, format="%(asctime
 # This line calls the function that downloads the admin tag table and stores it in memory.
 download_admin()
 
+# Possible identification status
+unknown = 0
+admin = 1
+confirmed_student = 2
+rejected_student = 3
+
 # The following blocks of code prepare the I/O pins of the Raspberry Pi for the RFID Reader.
-relay = 11
-red_led = 12
-buzzer = 13
+relay = 19
+red_led = 16
+buzzer = 26
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
@@ -139,6 +184,13 @@ GPIO.output(relay, GPIO.LOW)
 GPIO.output(red_led, GPIO.LOW)
 GPIO.output(buzzer, GPIO.LOW)
 
+# Configures the LCD screen
+lcd_columns = 16
+lcd_rows = 2
+i2c = busio.I2C(board.SCL, board.SDA)
+lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
+lcd.color = [100, 0, 0]
+
 # Hook the SIGINT.
 signal.signal(signal.SIGINT, end_read)
 
@@ -149,12 +201,19 @@ continue_reading = True
 
 while continue_reading:
 
+        if admin_table == True:
+        lcd.message = "Ready\nInsert Tag"
+        GPIO.output(green_led, GPIO.HIGH)
+    else:
+        lcd.message = "Comm Error\nRestart Pi"
+        GPIO.output(green_led, GPIO.LOW)
+
     # Scan for cards.
     (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
     # If a card is found.
     if status == MIFAREReader.MI_OK:
-        print "Card detected"
+        print ("Card detected")
 
     # Get the UID of the card.
     (status, uid) = MIFAREReader.MFRC522_Anticoll()
@@ -174,22 +233,29 @@ while continue_reading:
         # Check if authenticated.
         if status == MIFAREReader.MI_OK:
 
-            # Gets the Booked User ID from the tag (of type "list"), and converts it into an integer.
-            user_id_list = MIFAREReader.MFRC522_Read(8)
-            MIFAREReader.MFRC522_StopCrypto1()
-            user_id_int = int(''.join(str(e) for e in user_id_list))
-
-            # Converts the uid into an integer.
-            uid_int = int(''.join(str(e) for e in uid))
-
             try:
-                if admin_or_user(uid_int, user_id_int) == True:
+                # Gets the Booked User ID from the tag (of type "list"), and converts it into an integer.
+                user_id_list = MIFAREReader.MFRC522_Read(8)
+                MIFAREReader.MFRC522_StopCrypto1()
+                user_id_int = int(''.join(str(e) for e in user_id_list))
+
+                # Converts the uid into an integer.
+                uid_int = int(''.join(str(e) for e in uid))
+
+                # Gets the administrator token that we use to authenticate API calls.
+                auth_headers = get_headers()
+
+                reservation, identification = find_identification(uid_int, user_id_int, auth_headers)
+
+                if identification == admin or identification == confirmed_student:
+                    check_in(identification, user_id_int, reservation, auth_headers)
                     logging.info("The user with UID {} and BookedId {} has started a session".format(uid_int, user_id_int)) # Makes a log entry every time a user has been given access to the machine.
                     next_read = True
                     i = 0
 
                     while next_read:
-                        GPIO.output(red_led, GPIO.LOW)
+                        lcd.clear()
+                        lcd.message = "Authenticated\nDon't Remove Tag"
                         GPIO.output(relay, GPIO.HIGH)
 
                         # This is the time that the Pi will take before taking another reading.
@@ -205,12 +271,16 @@ while continue_reading:
                             i += 1
                         else:
                             critical_mode = True
-                            GPIO.output(red_led, GPIO.HIGH)
                             GPIO.output(buzzer, GPIO.HIGH)
                             time.sleep(3)       # Seconds that the buzzer remains on.
                             GPIO.output(buzzer, GPIO.LOW)
                             ii = 0
                             while critical_mode:
+                                lcd.clear()
+                                lcd.message = "Ending Session\nIn 60s"
+                                time.sleep(2.5)
+                                lcd.clear()
+                                lcd.message = "Continue?\nReinsert Tag"
                                 # Waits for 60 seconds in critical mode. If no valid tag is inserted, it goes back to initial loop.
                                 if(ii <= 6):
                                     (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
@@ -221,27 +291,36 @@ while continue_reading:
                                         i = 0
                                     else:
                                         ii += 1
-                                        time.sleep(10)
+                                        time.sleep(5)
                                 else:
                                     critical_mode = False
                                     next_read = False
                     GPIO.output(relay, GPIO.LOW)
-                    GPIO.output(red_led, GPIO.LOW)
+                    check_out(identification, user_id_int, reservation, auth_headers)
                 else:
-                    print "The time of the reservation does not match or you are not authorized to use this machine."
+                    if identification == unknown:  # When the tag is not registered in the system.
+                        print ("The tag is not registered on the system")
+                        lcd.clear()
+                        lcd.message = "Unrecognized Tag"
+                    elif identification == rejected_student: # When the user exists in our system but does not have a reservation in this machine at the time.
+                        print ("Currently, the user does not have any active reservations on this machine.")
+                        lcd.clear()
+                        lcd.message = "No Reservations\nFound"
 
-            except KeyError:      # Error associated with the "find_booked_uid" function. If the user is not registered in Booked.
-                print "The user does not exist in Booked"
-            except IndexError:     # Error associated with the "compare_reservations_time" function. If the user has no reservations then this error happens.
-                print "The user does not have any active reservations for this machine."
             except TypeError:
                 logging.info("There was a problem reading the BookedId of the tag.")     # Error that pops up ocasionally in the line "user_id_int".
-                raise
             except requests.exceptions.ConnectionError:     # Error occurs when the Pi cannot communicate with Booked (i.e there is no wi-fi or Booked is down).
                 logging.info("Communication with Booked could not be established.") # Makes a log entry for when there is no wi-fi.
-                print "There is a problem with the wi-fi connection. Ask the shop personnel for admin tags."
+                print ("There is a problem with the wi-fi connection. Ask the shop personnel for admin tags.")
+                GPIO.output(green_led, GPIO.LOW)
+                lcd.clear()
+                lcd.message = "No Wifi\nRestart Pi"
+            except Exception:   # Logs in all other unknown errors.
+                logging.exception("Unknown Error")
+                raise
 
             time.sleep(3)  # Time it takes for the reader to make another reading. Ensures the RFID module reads correctly.
+            lcd.clear()
 
         else:
-            print "Authentication error"
+            print ("Authentication error")
--- Access-MFRC522-python/WriteToTag.py
+++ Access-MFRC522-python/WriteToTag.py
@@ -1,5 +1,28 @@
-# Modified by Ricardo Rivera on May 1st, 2019
+# Created by Ricardo Rivera
+# Last Edit: Ricardo Rivera, August 12th 2019
+# The test table is called "mechStudents"
 
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+#
+#    Copyright 2014,2018 Mario Gomez <mario.gomez@teubi.co>
+#
+#    This file is part of MFRC522-Python
+#    MFRC522-Python is a simple Python implementation for
+#    the MFRC522 NFC Card Reader for the Raspberry Pi.
+#
+#    MFRC522-Python is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    MFRC522-Python is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with MFRC522-Python.  If not, see <http://www.gnu.org/licenses/>.
 # Modify all code you see written in CAPS with an underscore with your information. Be careful not to modify the SQL code that is also in caps.
 
 import RPi.GPIO as GPIO
@@ -7,16 +30,17 @@ import MFRC522
 import signal
 
 from sqlalchemy import create_engine
+from sqlalchemy.sql import text
 import pandas as pd
 import math
 import time
-#import requests
-#import json
+import requests
+import json
 
 # Capture SIGINT for cleanup when the script is aborted.
 def end_read(signal, frame):
     global continue_reading
-    print "Ctrl+C captured, ending read."
+    print ("Ctrl+C captured, ending read.")
     continue_reading = False
     GPIO.cleanup()
 
@@ -37,8 +61,8 @@ def authenticateBookedAdmin():
 engine = create_engine("mysql+pymysql://SQL_USERNAME:SQL_PASSWORD@MYSQL_SERVER_IP_ADDRESS/YOUR_DATABASE_NAME")
 
 # Finds the students in the table that do not have an UID and creates a pandas dataframe with these.
-call_to_table = engine.execute("SELECT id, GivenName, Surname, Email, BookedID, BookedUsername  FROM YOUR_USER_TABLE_NAME WHERE UID IS NULL").fetchall()
-table = pd.DataFrame(call_to_table, columns=["id", "GivenName", "Surname", "Email", "BookedID", "BookedUsername"])
+call_to_table = engine.execute("SELECT id, GivenName, Surname, Email, BookedID, BookedUsername, TagNum FROM mechStudents WHERE UID=0 AND Registered=0").fetchall()
+table = pd.DataFrame(call_to_table, columns=["id", "GivenName", "Surname", "Email", "BookedID", "BookedUsername", "TagNum"])
 
 # Hook the SIGINT
 signal.signal(signal.SIGINT, end_read)
@@ -49,7 +73,7 @@ MIFAREReader = MFRC522.MFRC522()
 # Main loop that will figure out which students in the table do not have a UID. It will then ask to place the tag near the reader to generate the StudentToken key.
 for i in table.index:
     row = table.iloc[i]
-    print "Quickly tap and remove the tag for %s with id %i on the reader now" % (row["GivenName"], row["id"])
+    print("Quickly tap and remove the tag {} for {} on the reader now".format(row["TagNum"],row["GivenName"]))
 
     continue_reading = True
 
@@ -61,7 +85,7 @@ for i in table.index:
 
         # If a card is found
         if status == MIFAREReader.MI_OK:
-            print "Card detected"
+            print ("Card detected")
 
         # Get the UID of the card
         (status, uid) = MIFAREReader.MFRC522_Anticoll()
@@ -77,7 +101,7 @@ for i in table.index:
 
             # Authenticate
             status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
-            print "\n"
+            print ("\n")
 
             # Check if authenticated
             if status == MIFAREReader.MI_OK:
@@ -92,28 +116,29 @@ for i in table.index:
                 for ii in str(row["BookedID"]):
                     data.append(int(ii))
 
-                print "Sector 8 looked like this:"
+                print ("Sector 8 looked like this:")
                 # Read block 8
                 MIFAREReader.MFRC522_Read(8)
-                print "\n"
+                print ("\n")
 
                 MIFAREReader.MFRC522_Write(8, data)
 
-                print "It now looks like this:"
+                print ("It now looks like this:")
                 # Check to see if it was written
                 MIFAREReader.MFRC522_Read(8)
-                print "\n"
+                print ("\n")
 
                 # We iterate over the list "uid" to create a number.
                 uid_int = int(''.join(str(e) for e in uid))
 
                 # Writes the tag's UID into the "mechStudents" table
-                engine.execute("UPDATE YOUR_USER_TABLE_NAME SET UID = %i WHERE GivenName = '%s'" % (uid_int, row["GivenName"]))
+                write_uid_command = text("UPDATE mechStudents SET UID = :v1 WHERE id = :v2")
+                engine.execute(write_uid_command, v1=uid_int, v2=int(row["id"]))
 
                 # The following commented block of code writes the UID corresponding to that user and tag in Booked. Commented out because
                 # "UpdateBooked.py" performs the same task. It is placed here for the reader to understand the following scripts.
                     #headers = authenticateBookedAdmin()
-                    #update_user_url = "http://ubcmechstudentmachineshop.brickhost.com/Web/Services/index.php/Users/%i" % row["BookedID"]
+                    #update_user_url = "http://YOUR_BOOKED_DOMAIN/Web/Services/index.php/Users/%i" % row["BookedID"]
                     #update_user_args = {"firstName": row["GivenName"], "lastName": row["Surname"], "emailAddress": row["Email"],
                     #"userName": row["BookedUsername"], "timezone": "America/Vancouver", "customAttributes": [{"attributeId": "10", "attributeValue": uid_int}]}
                     #update_user_args_str = json.dumps(update_user_args)
@@ -128,4 +153,4 @@ for i in table.index:
                 continue_reading = False
 
             else:
-                print "Authentication error"
+                print ("Authentication error")
